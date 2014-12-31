1a6e17900cbf3ddac0079d7acdf3e07d7872ecce
Jeong minchul <deajang@gmail.com>
2014-12-31 17:53:40 +0900

    test

--- src/main/java/net/indf/collection/net/indf/collection/block/Block.java
+++ src/main/java/net/indf/collection/net/indf/collection/block/Block.java
@@ -3,5 +3,9 @@ package net.indf.collection.net.indf.collection.block;
 /**
  * Created by 정민철 on 2014-12-26.
  */
-public interface Block {
+public interface Block<T> {
+    int count();
+    void add (T item);
+    void remove(T item);
+    boolean contains(T item);
 }
